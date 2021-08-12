8c0d889cb6f0da6bec703a0dd0c87eee0cc3a2c0
Valentyna Levkovska <78691453+vlevkovska@users.noreply.github.com>
2021-08-12 23:08:36 +0300

    js written

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import countryTpl from './templates/countries.hbs';
 import fetchCountries from './js/fetchCountries';
-const _ = require('lodash');
+import debounce from 'lodash.debounce';
 
 const refs = {
-  input: document.querySelector('#input'),
+  input: document.querySelector('#input-id'),
   output: document.querySelector('.country'),
 };
 
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
+import { defaults } from '@pnotify/core';
+defaults.maxTextHeight = null;
+
 // считывание с инпута
-refs.input.addEventListener('input', _.debounce(searchCountry, 500));
+refs.input.addEventListener('input', debounce(searchCountry, 500));
 // refs.input.addEventListener('input', searchCountry);
 
 function searchCountry(e) {
@@ -17,13 +23,18 @@ function searchCountry(e) {
   fetchCountries(searchQuery).then(data => {
     console.log(data);
     if (data.length > 10) {
+      error({
+        title: 'Excuse me!!',
+        text: 'Too many matches found. Please enter a more specific query!',
+        delay: 4000,
+      });
       console.log('Too many matches found...');
     } else if (data.length === 1) {
       const countryMarkup = countryTpl(data[0]);
       refs.output.innerHTML = countryMarkup;
     } else {
       const countryMarkup = data.map(country => `<li>${country.name}</li>`).join('');
-      refs.output.innerHTML = countryMarkup;
+      refs.output.innerHTML = `<ul class="country__list">${countryMarkup}</ul>`;
     }
   });
 }
